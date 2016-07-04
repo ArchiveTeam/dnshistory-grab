a20a0bf473ee25e708967e4a8321a910f3666be9
Arkiver2 <Arkiver@hotmail.com>
2016-07-04 15:49:39 +0200

    initial

---
+++ dnshistory.lua
@@ -0,0 +1,191 @@
+dofile("urlcode.lua")
+dofile("table_show.lua")
+
+local url_count = 0
+local tries = 0
+local item_type = os.getenv('item_type')
+local item_value = os.getenv('item_value')
+local item_tld = os.getenv('item_tld')
+local item_dir = os.getenv('item_dir')
+
+local downloaded = {}
+local addedtolist = {}
+local domains = {}
+
+for ignore in io.open("ignore-list", "r"):lines() do
+  downloaded[ignore] = true
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+fix_domain = function(domain)
+  if string.match(domain, "(.+)%.$") then
+    domain = string.match(domain, "(.+)%.$")
+  end
+  domain = string.match(domain, "([^%.]+%.[^%.]+)$")
+  return domain
+end
+
+get_domains = function(html)
+  for domain in string.gmatch(html, '"/dns%-records/([^"]+)"') do
+    domain = fix_domain(domain)
+    domains[domain] = true
+  end
+end
+
+allowed = function(url)
+  if string.match(url, "^https?://[^/]*dnshistory%.org/.+") then
+    for part in string.gmatch(string.match(url, "^https?://[^/]+/(.+)"), "([^/]+)") do
+      part = fix_domain(part)
+      if domains[part] then
+        return true
+      end
+    end
+  end
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+
+  if (downloaded[url] ~= true and addedtolist[url] ~= true) and (allowed(url) or html == 0) then
+    addedtolist[url] = true
+    return true
+  else
+    return false
+  end
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+
+  downloaded[url] = true
+  
+  local function check(urla)
+    local url = string.match(urla, "^([^#]+)")
+    if (downloaded[url] ~= true and addedtolist[url] ~= true) and allowed(url) then
+      if string.match(url, "&amp;") then
+        table.insert(urls, { url=string.gsub(url, "&amp;", "&") })
+        addedtolist[url] = true
+        addedtolist[string.gsub(url, "&amp;", "&")] = true
+      else
+        table.insert(urls, { url=url })
+        addedtolist[url] = true
+      end
+    end
+  end
+
+  local function checknewurl(newurl)
+    if string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^//") then
+      check("http:"..newurl)
+    elseif string.match(newurl, "^/") then
+      check(string.match(url, "^(https?://[^/]+)")..newurl)
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    if string.match(newurl, "^%?") then
+      check(string.match(url, "^(https?://[^%?]+)")..newurl)
+    elseif not (string.match(newurl, "^https?://") or string.match(newurl, "^/") or string.match(newurl, "^[jJ]ava[sS]cript:") or string.match(newurl, "^[mM]ail[tT]o:") or string.match(newurl, "^%${")) then
+      check(string.match(url, "^(https?://.+/)")..newurl)
+    end
+  end
+  
+  if allowed(url) or string.match(url, "^https://dnshistory.org/subdomains/[0-9]+/"..item_tld.."$") then
+    html = read_file(file)
+    if string.match(url, "^https://dnshistory.org/subdomains/[0-9]+/"..item_tld.."$") then
+      get_domains(html)
+    end
+    for newurl in string.gmatch(html, '([^"]+)') do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "([^']+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, ">([^<]+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "PopWin%('([^']+)") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, 'PopWin%("([^"]+)') do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, "href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, 'href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+  end
+
+  return urls
+end
+  
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  -- NEW for 2014: Slightly more verbose messages because people keep
+  -- complaining that it's not moving or not working
+  status_code = http_stat["statcode"]
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. ".  \n")
+  io.stdout:flush()
+
+  if downloaded[url["url"]] == true then
+    return wget.actions.EXIT
+  end
+
+  if (status_code >= 200 and status_code <= 399) then
+    if string.match(url.url, "https://") then
+      local newurl = string.gsub(url.url, "https://", "http://")
+      downloaded[newurl] = true
+    else
+      downloaded[url.url] = true
+    end
+  end
+  
+  if status_code >= 500 or
+    (status_code >= 401 and status_code ~= 404) or
+    status_code == 0 then
+    io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
+    io.stdout:flush()
+    os.execute("sleep 1")
+    tries = tries + 1
+    if tries >= 5 then
+      io.stdout:write("\nI give up...\n")
+      io.stdout:flush()
+      tries = 0
+      if allowed(url["url"]) then
+        return wget.actions.ABORT
+      else
+        return wget.actions.EXIT
+      end
+    else
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
